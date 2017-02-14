f9001b77dfd66f6706f0ac99dbe9be05796f6906
kimmabrandt <kimmabrandt@gmail.com>
2017-02-13 20:31:42 -0800

    api working, mongo working

--- mean-todo/routes/todos.js
+++ mean-todo/routes/todos.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Todo = require('../models/Todo.js');
 
- // GET /todos listing
+ // List: GET /todos listing
 router.get('/', function(req, res, next) {
   Todo.find(function (err, todos) {
     if (err) return next(err);
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// POST /todos
+// Create: POST /todos
 router.post('/', function(req, res, next) {
   Todo.create(req.body, function(err, post) {
     if(err) return next(err);
@@ -20,12 +20,30 @@ router.post('/', function(req, res, next) {
   });
 });
 
-/* GET /todos/id */
+// Show: GET /todos/id
 router.get('/:id', function(req, res, next) {
   Todo.findById(req.params.id, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
 });
+
+// Update: PUT /todos/:id
+router.put('/:id', function(req, res, next) {
+  Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Destroy: DELETE /todos/:id
+router.delete('/:id', function(req, res, next) {
+  Todo.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+
 
 module.exports = router;
